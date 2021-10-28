ebbbefde942181b7db4883bc77df75303b64323b
Cristhian Vargas Herhuay <edwarjhoel.99@gmail.com>
2021-10-28 12:08:57 -0500

    subiendo KthLargesElementinanArray.cpp y captura del juez online

--- SegundaUnidad/Aula13/KthLargestElementinanArray.cpp
+++ SegundaUnidad/Aula13/KthLargestElementinanArray.cpp
@@ -18,20 +18,23 @@
 
 using namespace std;
 
-
-
-
-
 int findKthLargest(vector<int>& nums, int k) {
-        
-
+    priority_queue<int> g;
+    for(int i=0 ; i<nums.size(); i++)
+        g.push(nums[i]);
+    int it;
+    while(k--)
+    {
+        it=g.top();
+        g.pop();
+    }
+    return it;
 }
 
 int main()
 {
     vector<int> nums={3,2,1,5,6,4};
-    int k;
-    cin>>k;
+    int k=2;
     cout<<findKthLargest(nums,k);
     return 0;
 
