fb40dcb0e982e392f0051ca31a2d34c43dc226d8
Cristhian Vargas Herhuay <edwarjhoel.99@gmail.com>
2021-11-07 10:58:48 -0500

    subiendo makingalargeIsland a aula15

--- SegundaUnidad/Aula15/MakingaLargeIsland.cpp
+++ SegundaUnidad/Aula15/MakingaLargeIsland.cpp
@@ -1,7 +1,6 @@
-/* 
-https://leetcode.com/problems/making-a-large-island/
+/* https://leetcode.com/problems/making-a-large-island/
  */
-/* #include <bits/stdc++.h>
+#include <bits/stdc++.h>
 #include <numeric>
 #include <map>
 #include <cmath>
@@ -14,50 +13,102 @@ https://leetcode.com/problems/making-a-large-island/
 #include <algorithm>
 #include <stack>
 #include <climits>
-
-
+#include <random>
+#include <time.h>
 using namespace std;
 
-int DFS(vector<vector<int>> &grid,int i,int j,int numero)
+
+
+void dfs(vector<vector<int>>& grid, int i, int j, int idx, int& count)
 {
-    if (i < 0 || j < 0 || i >= grid[0].size() || j >= grid.size() || grid[i][j]!=1) 
-        return 0;
-
-
-    else{
-        return 1  + DFS(grid,i+1, j,numero) + DFS(grid,i, j-1,numero) 
-                    + DFS(grid,i, j+1,numero) + DFS(grid,i-1, j,numero);
+    if(i < 0 || j < 0 || i >= grid.size() || j >= grid[0].size() || grid[i][j] != 1)
+    {
+        return;
     }
+    
+    grid[i][j] = idx;
+    ++count;
+    dfs(grid, i + 1, j, idx, count);
+    dfs(grid, i - 1, j, idx, count);
+    dfs(grid, i, j + 1, idx, count);
+    dfs(grid, i, j - 1, idx, count);
 }
 
-
-int floodfill(vector<vector<int>> &grid)
+int largestIsland(vector<vector<int>>& grid) 
 {
-    priority_queue<int> q;
-    for(int i=0 ; i<grid.size() ; i++)
+    int idx = 2, totalArea = 0, maxArea = 0;
+    unordered_map<int, int> cnt;
+    for(int i = 0; i != grid.size(); ++i)
     {
-        int maxi=0;
-        for(int j=0 ; j<grid[0].size() ; j++)
+        for(int j = 0; j != grid[0].size(); ++j)
         {
-            if(grid[i][j]!=0)
+            if(grid[i][j] == 1)
             {
-                maxi=max(maxi,DFS(grid,i,j,grid[i][j]));
-                q.push(maxi);
+                int count = 0;
+                dfs(grid, i, j, idx, count);
+                cnt[idx++] = count;
+                totalArea += count;
+                maxArea = max(maxArea, count + 1);
             }
         }
     }
-
-    return q.top();
+    
+    if(totalArea == 0)
+    {
+        return 1;
+    }
+    
+    if(totalArea == grid.size() * grid[0].size())
+    {
+        return totalArea;
+    }
+    
+    for(int i = 0; i != grid.size(); ++i)
+    {
+        for(int j = 0; j != grid[0].size(); ++j)
+        {
+            unordered_set<int> visited;
+            int area = 1;
+            if(i > 0 && grid[i - 1][j] != 0 && visited.find(grid[i - 1][j]) == visited.end())
+            {
+                visited.insert(grid[i - 1][j]);
+                area += cnt[grid[i - 1][j]];
+            }
+            if(i < grid.size() - 1 && grid[i + 1][j] != 0 && visited.find(grid[i + 1][j]) == visited.end())
+            {
+                visited.insert(grid[i + 1][j]);
+                area += cnt[grid[i + 1][j]];
+            }
+            if(j > 0 && grid[i][j - 1] != 0 && visited.find(grid[i][j - 1]) == visited.end())
+            {
+                visited.insert(grid[i][j - 1]);
+                area += cnt[grid[i][j - 1]];
+            }
+            if(j < grid[0].size() - 1 && grid[i][j + 1] != 0 && visited.find(grid[i][j + 1]) == visited.end())
+            {
+                visited.insert(grid[i][j + 1]);
+                area += cnt[grid[i][j + 1]];
+            }
+            maxArea = max(area, maxArea);
+        }
+    }
+    
+    return maxArea;
 }
+
+
 
 int main()
 {
-    vector<vector<int>> grid {{1,0},
-                                {0,1}};
+    vector<vector<int>> grid   {{0,0,0,0,0,0,0},
+                                {0,1,1,1,1,0,0},
+                                {0,1,0,0,1,0,0},
+                                {1,0,1,0,1,0,0},
+                                {0,1,0,0,1,0,0},
+                                {0,1,0,0,1,0,0},
+                                {0,1,1,1,1,0,0}};
 
-    cout<<floodfill(grid);
-
+    cout<<largestIsland(grid);
 
 
 }
- */
