b65db54c41f0e2056ec56367234e883acc108ad6
Cristhian Vargas Herhuay <edwarjhoel.99@gmail.com>
2021-11-26 11:16:49 -0500

    creando carpeta examen2pc y subiendo P8_Fibos'sSequence

---
+++ SegundaUnidad/Examen2PC/P1_Fibos'sSequence/P8_Fibos'sSequence.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+#include <cstdio>
+
+
+using namespace std;
+
+int fib(int n)
+{
+    if (n <= 1)
+        return n;
+    return fib(n-1) + fib(n-2);
+}
+
+int main() {
+    long long a, b, n;
+    long long num;
+
+    while(true) {
+        num = 0;
+
+        scanf("%d%d%d", &a, &b, &n);
+        if((a == 0 && b == 0 && n == 0))
+            break;
+
+        for(int cont = 0 ; cont < n; cont++) {
+            num += fib(a) * fib(b);
+            a++;b++;
+        }
+        printf("%d\n",num);
+    }
+
+    return 0;
+}
