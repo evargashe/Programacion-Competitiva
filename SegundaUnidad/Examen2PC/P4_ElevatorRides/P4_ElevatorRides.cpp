f66be4aa69706c1734b3c4f38bb4e992f85b2fc4
Cristhian Vargas Herhuay <edwarjhoel.99@gmail.com>
2021-11-27 23:11:48 -0500

    ctualizando P4_ElevatorRides

--- SegundaUnidad/Examen2PC/P4_ElevatorRides/P4_ElevatorRides.cpp
+++ SegundaUnidad/Examen2PC/P4_ElevatorRides/P4_ElevatorRides.cpp
@@ -6,30 +6,30 @@ https://cses.fi/problemset/task/1653/
 #include<bits/stdc++.h>
 using namespace std;
 
-#define int long long
+#define ll long long
 
-signed main(){
+int main(){
     
-    int n, k; cin>>n>>k;
-    int a[n];
-    for (int i = 0; i < n; i++)
-        cin>>a[i];
-    pair<int, int> dp[1<<n];
-    dp[0] = {0, k+1};
-    for (int s = 1; s < (1<<n); s++) {
-        dp[s] = {25, 0};
-        for (int i = 0; i < n; i++) {
+    ll n, k; cin>>n>>k;
+    vector<ll> matrix(n,0);
+    for (ll i = 0; i < n; i++)
+        cin>>matrix[i];
+    pair<ll, ll> pr[1<<n];
+    pr[0] = {0, k+1};
+    for (ll s = 1; s < (1<<n); s++) {
+        pr[s] = {25, 0};
+        for (ll i = 0; i < n; i++) {
             if (s>>i&1){
-                auto [c, w] = dp[s^(1<<i)];
-                if (w + a[i] > k) {
+                auto [c, w] = pr[s^(1<<i)];
+                if (w + matrix[i] > k) {
                     c++;
-                    w = min(a[i], w);
+                    w = min(matrix[i], w);
                 }
                 else
-                    w += a[i];
-                dp[s] = min(dp[s], {c, w});
+                    w += matrix[i];
+                pr[s] = min(pr[s], {c, w});
             }
         }
     }
-    cout<<dp[(1<<n)-1].first;
+    cout<<pr[(1<<n)-1].first;
 }
