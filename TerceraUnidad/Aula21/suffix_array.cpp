760ebfea56eebe7dccd7c025d49056391411e135
Cristhian Vargas Herhuay <edwarjhoel.99@gmail.com>
2021-12-05 22:55:57 -0500

    creando carpeta Tercera Unidad y creando carpeta aula21 y subiendo ejercicios a aula21

---
+++ TerceraUnidad/Aula21/suffix_array.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<int> SuffixArray(string text){
+    
+    int n = text.size();
+    map<string,int> order_id;
+    
+    vector<int> suffix(n);
+
+    for (size_t i = 0; i < n; i++) {
+        // cout<<text.substr(i, n)<<" "<<i<<"\n";
+        order_id[text.substr(i, n)] = i;
+    }
+    int j = 0;
+    for (auto x : order_id) {
+        suffix[j] = x.second;
+        j++;
+    }
+    return suffix;
+
+}
+
+int main(){
+
+    string text;
+    cin >> text;
+    vector<int> Suffixarr = SuffixArray(text);
+     
+    
+    for (size_t i = 0; i < Suffixarr.size(); i++) {
+        cout << Suffixarr[i] << " ";
+    }
+
+    return 0;
+}
+
