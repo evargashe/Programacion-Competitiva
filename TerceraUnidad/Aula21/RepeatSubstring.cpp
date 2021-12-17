f045e46b3a4024f698aa00ce33b66fdacdb4ae2c
Cristhian Vargas Herhuay <edwarjhoel.99@gmail.com>
2021-12-17 00:03:24 -0500

    subiendo aula21 repeatSubstring

--- TerceraUnidad/Aula21/RepeatSubstring.cpp
+++ TerceraUnidad/Aula21/RepeatSubstring.cpp
@@ -1,110 +1,90 @@
+/* https://open.kattis.com/problems/substrings
+ */
 #include <bits/stdc++.h>
 
 using namespace std;
-class SuffixArray{
-    public:
-        static const int max_n = 100010;
-        int n =-1;
-        string str;
 
-        int RA[max_n], SA[max_n];
-        int tempRA[max_n], tempSA[max_n];
-        int* lcp;
 
-        void countingSort(int k){
-            int i;
-            int maxi = max(300,n);
-            int sum = 0;
-            vector<int> c(max_n);
+int MAX_N = 100010;
+int n = -1;
+string s;
+vector<int> RA, SA, tempRA, tempSA, lcp;
 
-            for (i = 0; i < n; i++) c[i+k< n ? RA[i+k] : 0]++;
-            for (i = 0; i < maxi; i++)
-            {
-                int t = c[i];
-                c[i] = sum;
-                sum+=t;
-            }
 
-            for (i = 0; i < n; i++)
-            {
-                tempSA[c[SA[i] + k<n ? RA[SA[i] + k] : 0]++ ] = SA[i];
-            }
-            for ( i = 0; i < n; i++)
-            {
-                SA[i] = tempSA[i];
-            }
+void Fx(){
+    int k = 0;
+    lcp.resize(n);
+    int *rank = new int[n];
+    for (int i = 0; i < n; i++) 
+        rank[SA[i]] = i;
+    for (int i = 0; i < n; i++, k  = max(k-1, 0)){
+        if (rank[i] == n - 1) {
+            k = 0;
+            continue;
         }
-
-        void kasai(){
-            int k = 0;
-            this->lcp = new int[n];
-            vector<int> rank(n);
-            for (int i = 0; i < n; i++) rank[this->SA[i]] = i;
-
-            for (int  i = 0; i < n; i++ , k = max(k-1,0))
-            {
-                if (rank[i] == n-1)
-                {
-                    k = 0;
-                    continue;
-                }
-                int j = this->SA[rank[i]+1];
-                while (i+k<n && j+k < n && str[i+k] == str[j+k]) k++;
-                this->lcp[rank[i]] = k;
-                
-            }
-        }
-        SuffixArray(string x){
-            this->str = x;
-            this->str += "$";
-            this->n = str.length();
-            for (int i = 0; i < n; i++) RA[i] = str[i];
-            for (int i = 0; i < n; i++) SA[i] = i;
-            
-            for (int k = 1; k < n; k *=2)
-            {
-                countingSort(k);
-                countingSort(0);
-                int r = 0;
-                tempRA[SA[0]] = r;
-                for (int i = 1; i < n; i++)
-                {
-                    tempRA[this->SA[i]] = (RA[this->SA[i]] == RA[this->SA[i-1]] && RA[this->SA[i] + k] == RA[this->SA[i-1] + k]) ? r : ++r;    
-                }
-                for (int i = 0; i < n; i++)
-                {
-                    RA[i] = tempRA[i];
-                }
-
-                if (RA[this->SA[n-1]] == n-1) break;
-            }
-            kasai();
-            
-        }
-       
-};
-
-
-int main(){
-
-    int T;
-    cin>>T;
-    int nm = 1;
-    string text;
-    while (cin>>text)
-    {
-        SuffixArray* sa = new SuffixArray(text);
-        
-        int ans = sa->lcp[0];
-        for (int i = 1; i < sa->n; i++)
-        {
-            if (sa->lcp[i] > sa->lcp[i-1]) ans += (sa->lcp[i] - sa->lcp[i-1]);
-        }
-        // printf("%ld\n", ans);
-        cout<<ans<<endl;
-        T--;
-        if (T <= 0) break;
+        int j = SA[rank[i] + 1];
+        while (i + k < n && j + k < n && s[i + k] == s[j + k]) k++;
+        lcp[rank[i]] = k;
     }
-       
+}
+
+void countingSort(int k){
+    int i, maxi = max(300, n);
+    int sum = 0;
+    int *c = new int[100010];
+    for (i = 0; i < n; i++) 
+        c[i + k < n ? RA[i + k] : 0]++;
+    for (i = 0; i < maxi; i++) { 
+        int t = c[i]; c[i] = sum; sum += t;
+    }
+    for (i = 0; i < n; i++) 
+        tempSA[c[SA[i] + k < n ? RA[SA[i] + k] : 0]++] = SA[i];
+    for (i = 0; i < n; i++) 
+        SA[i] = tempSA[i];
+}
+
+void RepeatedSubstring(string x){
+    RA.resize(MAX_N), SA.resize(MAX_N), tempRA.resize(MAX_N), tempSA.resize(MAX_N);
+
+    s = x;
+    s += '$';
+    n = s.length();
+    int i, k;
+    for (i = 0; i < n; i++) RA[i] = s[i];
+    for (i = 0; i < n; i++) SA[i] = i;
+    for (k = 1; k < n; k *= 2){
+        countingSort(k);
+        countingSort(0);
+        int r = 0;
+        tempRA[SA[0]] = r;
+        for (i = 1; i < n; i++){
+            tempRA[SA[i]] = (RA[SA[i]] == RA[SA[i - 1]] && RA[SA[i] + k] == RA[SA[i - 1] + k]) ? r : ++r;
+        }
+        for (i = 0; i < n; i++) 
+            RA[i] = tempRA[i];
+        if (RA[SA[n - 1]] == n - 1) 
+            break;
+    }   
+    Fx();
+}
+
+   
+int main()
+{
+    int t;
+    cin >> t;
+    while(t--){
+        string s;
+        cin >> s;
+        RepeatedSubstring(s);
+        int ans = lcp[0], i;
+        for(i = 1; i < n; i++){
+            if (lcp[i] > lcp[i-1]){
+                ans += (lcp[i] - lcp[i-1]);
+            }
+        }
+        cout << ans << endl;
+    }
+
     return 0;
 }
